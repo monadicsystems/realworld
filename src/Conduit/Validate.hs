eff3f2eed0d0a4d590140f72b9674034fff7c20d
Rashad Gover <rashad.sasaki@gmail.com>
2022-02-03 02:02:17 +0000

    Add more form checkers

--- src/Conduit/Validate.hs
+++ src/Conduit/Validate.hs
@@ -8,6 +8,7 @@ import Conduit.Model (SignInForm (..), SignUpForm (..))
 import Control.Applicative ((<|>))
 import Control.Monad ((>=>))
 import Control.Monad.Trans.Except
+import Data.Maybe (isNothing)
 import Data.Text (Text)
 import qualified Data.Text as T
 import Web.Forma
@@ -19,14 +20,14 @@ type SignUpFormFields = '["email", "password", "username"]
 signInForm :: Monad m => FormParser SignInFormFields (Text -> Text) m SignInForm
 signInForm =
   SignInForm
-    <$> field #email notEmpty
-    <*> field #password notEmpty
+    <$> field #email (notEmpty >=> validEmail)
+    <*> field #password (notEmpty >=> tooShort)
 
 signUpForm :: Monad m => FormParser SignUpFormFields (Text -> Text) m SignUpForm
 signUpForm =
   SignUpForm
-    <$> field #email notEmpty
-    <*> field #password notEmpty
+    <$> field #email (notEmpty >=> validEmail)
+    <*> field #password (notEmpty >=> tooShort)
     <*> field #username notEmpty
 
 notEmpty :: Monad m => Text -> ExceptT (Text -> Text) m Text
@@ -39,4 +40,10 @@ tooShort :: Monad m => Text -> ExceptT (Text -> Text) m Text
 tooShort txt =
   if T.length txt < 6
     then throwE (<> " must be 6 characters or more")
+    else pure txt
+
+validEmail :: Monad m => Text -> ExceptT (Text -> Text) m Text
+validEmail txt =
+  if isNothing $ T.find (== '@') txt
+    then throwE (<> " must contain @ symbol to be valid")
     else pure txt
