469fb43e00fd6d2e2fea5cde7438fbbc5c4fae48
Rashad Gover <rashad.sasaki@gmail.com>
2022-02-09 22:20:07 +0000

    Introduce hasql

--- src/Conduit/Database.hs
+++ src/Conduit/Database.hs
@@ -1,3 +1,22 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Conduit.Database where
 
 import Conduit.Model
+import qualified Hasql.TH as TH -- from "hasql-th"
+import Hasql.Statement
+import Data.Int
+
+sumStatement :: Statement (Int64, Int64) Int64
+sumStatement =
+  [TH.singletonStatement|
+    select ($1 :: int8 + $2 :: int8) :: int8
+    |]
+
+divModStatement :: Statement (Int64, Int64) (Int64, Int64)
+divModStatement =
+  [TH.singletonStatement|
+    select
+      (($1 :: int8) / ($2 :: int8)) :: int8,
+      (($1 :: int8) % ($2 :: int8)) :: int8
+    |]
